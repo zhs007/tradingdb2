20863ee78b95a802343c22001f2c84c41bcbac63
zhs007 <sssxueren@gmail.com>
2020-12-18 12:07:47 +0800

    - Fixed some bugs

--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,22 @@
 rm -rf grpc/tradingdb2
 rm -rf grpc/jrj
-rm -rf grpc/bitmet
+rm -rf grpc/bitmex
 rm -rf grpc/simtrading
 rm -rf unittestdata/tradingdb2
 rm -rf unittestdata/jrj
-rm -rf unittestdata/bitmet
+rm -rf unittestdata/bitmex
 rm -rf unittestdata/simtrading
 rm -rf utils/*.log
+
 go test -coverprofile=coverage.out ./...
 go tool cover -html=coverage.out
+
+rm -rf grpc/tradingdb2
+rm -rf grpc/jrj
+rm -rf grpc/bitmex
+rm -rf grpc/simtrading
+rm -rf unittestdata/tradingdb2
+rm -rf unittestdata/jrj
+rm -rf unittestdata/bitmex
+rm -rf unittestdata/simtrading
+rm -rf utils/*.log
