7933d97acb89cc5f6e91b1c8d298e192b25f1ffb
zhs007 <sssxueren@gmail.com>
2021-04-08 10:57:57 +0000

    Updated protos

--- buildprotos.sh
+++ buildprotos.sh
@@ -1,4 +1,4 @@
-# export PATH="$PATH:$(go env GOPATH)/bin"
+export PATH="$PATH:$(go env GOPATH)/bin"
 # export PATH="$PATH:/usr/local/go/bin"
 protoc --proto_path=protos/ --go_out=./tradingpb --go_opt=paths=source_relative protos/*.proto
 protoc --proto_path=protos/ --go-grpc_out=./tradingpb --go-grpc_opt=paths=source_relative protos/*.proto
