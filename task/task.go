e8e0683c72eb58c0a17920182944fc052392c048
zhs007 <sssxueren@gmail.com>
2021-05-07 12:08:14 +0000

    Updated task

--- task/task.go
+++ task/task.go
@@ -2,11 +2,15 @@ package tradingdb2task
 
 import "github.com/zhs007/tradingdb2/tradingpb"
 
-// FuncOnTaskEnd - used in BatchCandles
+// FuncOnTaskEnd - on task end
 type FuncOnTaskEnd func(task *Task) error
+
+// FuncOnTaskStart - on task start
+type FuncOnTaskStart func(task *Task) error
 
 // Task - Task
 type Task struct {
 	Params  *tradingpb.SimTradingParams
 	lstFunc []FuncOnTaskEnd
+	StartTs int64
 }
--- task/tasksmgr.go
+++ task/tasksmgr.go
@@ -1,6 +1,7 @@
 package tradingdb2task
 
 import (
+	"bytes"
 	"sync"
 
 	"github.com/zhs007/tradingdb2/tradingpb"
@@ -11,14 +12,16 @@ import (
 
 // TasksMgr - TasksMgr
 type TasksMgr struct {
-	MapTasks map[interface{}]*Task
-	mutex    sync.Mutex
+	mapTasks   map[interface{}]*Task
+	mutex      sync.Mutex
+	lstKeys    []interface{}
+	lstRunning []interface{}
 }
 
 // NewTasksMgr - new TasksMgr
 func NewTasksMgr() *TasksMgr {
 	return &TasksMgr{
-		MapTasks: make(map[interface{}]*Task),
+		mapTasks: make(map[interface{}]*Task),
 	}
 }
 
@@ -32,7 +35,7 @@ func (mgr *TasksMgr) HasTask(params *tradingpb.SimTradingParams) bool {
 	}
 
 	mgr.mutex.Lock()
-	_, isok := mgr.MapTasks[buf]
+	_, isok := mgr.mapTasks[buf]
 	mgr.mutex.Unlock()
 
 	return isok
@@ -51,27 +54,57 @@ func (mgr *TasksMgr) AddTask(params *tradingpb.SimTradingParams, onEnd FuncOnTas
 	defer mgr.mutex.Unlock()
 
 	if onEnd == nil {
-		_, isok := mgr.MapTasks[buf]
+		_, isok := mgr.mapTasks[buf]
 		if !isok {
-			mgr.MapTasks[buf] = &Task{
+			mgr.mapTasks[buf] = &Task{
 				Params: params,
 			}
+
+			mgr.lstKeys = append(mgr.lstKeys, buf)
 		}
 
 		return nil
 	}
 
-	task, isok := mgr.MapTasks[buf]
+	task, isok := mgr.mapTasks[buf]
 	if isok {
 		task.lstFunc = append(task.lstFunc, onEnd)
 	} else {
-		mgr.MapTasks[buf] = &Task{
+		mgr.mapTasks[buf] = &Task{
 			Params:  params,
 			lstFunc: []FuncOnTaskEnd{onEnd},
 		}
+
+		mgr.lstKeys = append(mgr.lstKeys, buf)
 	}
 
 	return nil
+}
+
+// func (mgr *TasksMgr) delKey(buf []byte) {
+// 	for i, v := range mgr.lstKeys {
+// 		bv, isok := v.([]byte)
+// 		if isok {
+// 			if bytes.Equal(bv, buf) {
+// 				mgr.lstKeys = append(mgr.lstKeys[:i], mgr.lstKeys[i+1:]...)
+
+// 				return
+// 			}
+// 		}
+// 	}
+// }
+
+func (mgr *TasksMgr) delRunning(buf []byte) {
+	for i, v := range mgr.lstRunning {
+		bv, isok := v.([]byte)
+		if isok {
+			if bytes.Equal(bv, buf) {
+				mgr.lstRunning = append(mgr.lstRunning[:i], mgr.lstRunning[i+1:]...)
+
+				return
+			}
+		}
+	}
 }
 
 func (mgr *TasksMgr) OnTaskEnd(params *tradingpb.SimTradingParams) error {
@@ -86,13 +119,34 @@ func (mgr *TasksMgr) OnTaskEnd(params *tradingpb.SimTradingParams) error {
 	mgr.mutex.Lock()
 	defer mgr.mutex.Unlock()
 
-	task, isok := mgr.MapTasks[buf]
+	task, isok := mgr.mapTasks[buf]
 	if isok {
 		for _, v := range task.lstFunc {
 			v(task)
 		}
 
-		delete(mgr.MapTasks, buf)
+		delete(mgr.mapTasks, buf)
+		mgr.delRunning(buf)
+	}
+
+	return nil
+}
+
+func (mgr *TasksMgr) StartTask(onStart FuncOnTaskStart) error {
+	mgr.mutex.Lock()
+	defer mgr.mutex.Unlock()
+
+	for len(mgr.lstKeys) > 0 {
+		buf := mgr.lstKeys[0]
+		mgr.lstKeys = mgr.lstKeys[1:]
+
+		task, isok := mgr.mapTasks[buf]
+		if isok {
+			mgr.lstRunning = append(mgr.lstRunning, buf)
+			onStart(task)
+
+			return nil
+		}
 	}
 
 	return nil
